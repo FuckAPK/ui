1eab1861183698f3c75070614fd4385255896b2f
lyaaz <45838887+lyaaz@users.noreply.github.com>
2025-05-11 10:57:39 +0800

    fix: remove deprecated

--- src/main/java/org/lyaaz/ui/theme/Theme.kt
+++ src/main/java/org/lyaaz/ui/theme/Theme.kt
@@ -279,7 +279,6 @@ fun AppTheme(
     if (!view.isInEditMode) {
         SideEffect {
             val window = (view.context as Activity).window
-            window.statusBarColor = colorScheme.background.toArgb()
             WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = !darkTheme
         }
     }
