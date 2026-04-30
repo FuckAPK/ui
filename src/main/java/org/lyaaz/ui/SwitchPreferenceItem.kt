463800e519d80c8793b56d9cd86ce2ed20f5aa0b
lyaaz <45838887+lyaaz@users.noreply.github.com>
2026-04-30 08:01:50 +0800

    fix: add minimum interactive component size import

--- src/main/java/org/lyaaz/ui/SwitchPreferenceItem.kt
+++ src/main/java/org/lyaaz/ui/SwitchPreferenceItem.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
+import androidx.compose.material3.minimumInteractiveComponentSize
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
